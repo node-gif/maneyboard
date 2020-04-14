620691ee9e9e371138b4450803c9402322d6cac5
node-gif <at-resort.0404@outlook.jp>
2020-04-14 05:35:01 +0000

    掲示板一覧ページをルートに指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'boards/index'
-  get 'boards/show'
-  get 'boards/new'
-  get 'boards/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "boards#index"
+  
+  resources :boards
 end
