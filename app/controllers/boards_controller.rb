bb3c745a490afc7fadfc54c41876481eb85a1bb7
node-gif <at-resort.0404@outlook.jp>
2020-04-14 12:47:16 +0000

    boardsコントローラのCRUDを仮実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,26 @@
 class BoardsController < ApplicationController
+  # 掲示板一覧
   def index
+    @boards = Board.all
   end
 
+  # 掲示板詳細
   def show
+    @board = Board.find(board_params)
   end
 
+  # 新規投稿画面
   def new
+    @board = Board.new
   end
-
+  
+  # 編集画面
   def edit
+    @board = Board.find(board_params)
+  end
+  
+  # ストロングパラメーター
+  def board_params
+    params.require(:board).permit(:body, :title)
   end
 end
