3dde231ef83e6b6ee9a75cba609abe2e2e1bd808
node-gif <at-resort.0404@outlook.jp>
2020-04-14 12:56:27 +0000

    user_idを外部キーに持つBoardモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :baords
+         
   validates :name, presence: true, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_075009) do
+ActiveRecord::Schema.define(version: 2020_04_14_125357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_04_14_075009) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boards", "users"
 end
