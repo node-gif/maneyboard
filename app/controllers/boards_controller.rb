3b01748ade5b9e765b1c45e02190a9a20ccbfc73
node-gif <at-resort.0404@outlook.jp>
2020-04-14 13:50:33 +0000

    boardsコントローラの新規投稿機能を実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,7 +6,7 @@ class BoardsController < ApplicationController
 
   # 掲示板詳細
   def show
-    @board = Board.find(board_params)
+    @board = Board.find(params[:id])
   end
 
   # 新規投稿画面
@@ -14,13 +14,28 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
   
-  # 編集画面
-  def edit
-    @board = Board.find(board_params)
+  # 新規投稿機能
+  def create
+    # 受け取るのは:titleと:body
+    @board = Board.new(board_params)
+    # user_idは個別に追加
+    @board.user_id = current_user.id
+    if @board.save
+      flash[:notice] = "「#{ @board.title }」を投稿しました"
+      redirect_to boards_path
+    else
+      render :new
+    end
   end
   
-  # ストロングパラメーター
-  def board_params
-    params.require(:board).permit(:body, :title)
+  # 編集画面
+  def edit
+    @board = Board.find(params[:id])
   end
+  
+  private
+    # ストロングパラメーター
+    def board_params
+      params.require(:board).permit(:body, :title)
+    end
 end
